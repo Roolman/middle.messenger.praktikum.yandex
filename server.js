d9f892f08d6eeb0d664789c361e0adb08383347e
Roolman <roman-savelev4@yandex.ru>
2021-10-25 18:18:52 +0300

    Добавлены eslint и stylelint. Фикс ошибок.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 
-app.use(express.static(__dirname + "/dist"))
+app.use(express.static(`${__dirname}/dist`))
 
 const port = 3000
 
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html")
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`)
 })
 
 app.listen(port, () => {
-  console.log("Server started on port " + port)
+  console.log(`Server started on port ${port}`)
 })
