a63aeca2d18f88d217c76ef4a4f8f3fb5c86245a
Roolman <roman-savelev4@yandex.ru>
2021-11-18 11:02:43 +0300

    fixed login validator

--- src/constants/validators.ts
+++ src/constants/validators.ts
@@ -13,7 +13,7 @@ export const EMAIL_VALIDATOR: Validator = {
 
 export const LOGIN_PATTERN_VALIDATOR: Validator = {
     type: VALIDITY_TYPES.pattern,
-    value: "^[A-ZА-Я]{1}[A-Za-zА-Яа-я0-9\_\-]+$",
+    value: "^[A-Za-zA-Zа-я]{1}[A-Za-zА-Яа-я0-9\_\-]+$",
     error: "Логин должен содержать только символы [A-z][0-9]-_ и начинаться с буквы",
 }
 
