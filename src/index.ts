02dd93e7fcbae867b3e324807890000dc28aee51
Roolman <roman-savelev4@yandex.ru>
2021-11-22 18:20:25 +0300

    husky commit test

--- src/index.ts
+++ src/index.ts
@@ -66,5 +66,6 @@ Handlebars.registerHelper(
 document.addEventListener("invalid", (function () {
     return function (e: Event) {
         e.preventDefault()
+        console.log("test")
     }
 }()), true)
